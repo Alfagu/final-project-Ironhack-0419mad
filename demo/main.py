147b92b40df06cff1547694fdce9211ed0ad5eae
Alfon <alfonso_aguado@hotmail.es>
2019-10-22 21:21:45 +0200

    onit

--- demo/main.py
+++ demo/main.py
@@ -1,6 +1,12 @@
-from gui import *
+import gui
 from recom_sys import *
+import pandas as pd
 
+def main():
+    clicked, save_entry_fields = var_1
+    input = var_1 
+
+    print(get_recommendations(consult))
 
 if __name__ == "__main__":
     # Execute only if run as script
